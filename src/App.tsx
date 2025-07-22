fd3dfa218e0cd5927bb4bc313031813a1a16bab9
Jefferson Perluxo Clemente <jperluxo@gmail.com>
2025-07-21 22:05:35 -0300

    Implementação de componentes de formulário e listagem de tarefas (Parte 3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
 
 import Header from "./components/Header";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 import Footer from "./components/Footer";
 
+import { ITask } from "./interfaces/Task";
+
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header/>
         <main className={styles.main}>
-          <h2>Conteúdo...</h2>
+          <div>
+            <h2>O que você vai fazer?</h2>
+            <TaskForm btnText="Criar Tarefa" taskList={taskList} setTaskList={setTaskList}/>
+          </div>
+          <div>
+            <h2>Suas tarefas:</h2>
+            <TaskList taskList={taskList}/>
+          </div>
         </main>
       <Footer/>
     </div>
